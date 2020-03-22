b3644ec55806f4f9329f29691000fc245229b7ed
masterfermin02 <fperdomo@intellisys.com.do>
2020-03-22 12:50:57 -0400

    Added map tool tip

--- src/components/SVGMap.vue
+++ src/components/SVGMap.vue
@@ -8,15 +8,43 @@
                     <div class="mapsvg-scrollpane" style="transform: translate(0px, 0px);">
                         <div class="mapsvg-layers-wrap"><div class="mapsvg-layer mapsvg-layer-markers"></div>
                             <div class="mapsvg-layer mapsvg-layer-popovers"><div class="mapsvg-popover" style="width: 300px; max-width: 50%; max-height: 258.305px;"></div></div></div>
-    <svg xmlns:mapsvg="http://mapsvg.com" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" mapsvg:geoViewBox="-72.004173 19.932499 -68.322347 17.470139" preserveAspectRatio="xMidYMid meet" viewBox="0 0 792.71484 556.42358" style="transform: scale(1);">
-        <path v-for="(province, i) in provinces" :key="i" :index="i" :d="province.data" :title="province.title" :id="province.id" @mouseover="province.hover = true" @mouseleave="province.hover = false" class="mapsvg-region" :style="getStyle(province)"></path>
+    <svg id="map-svg" xmlns:mapsvg="http://mapsvg.com" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" mapsvg:geoViewBox="-72.004173 19.932499 -68.322347 17.470139" preserveAspectRatio="xMidYMid meet" viewBox="0 0 792.71484 556.42358" style="transform: scale(1);">
+        <path v-for="(province, i) in provinces"
+              :key="i"
+              :index="i"
+              :d="province.data"
+              :title="province.title"
+              :id="province.id"
+              @mouseover="mouseOver(province)"
+              @mouseleave="mouseLeave(province)"
+              class="mapsvg-region"
+              :style="getStyle(province)">
+        </path>
     </svg></div>
      <div class="mapsvg-loading" style="margin-left: -50.6955px; margin-top: -17px; display: none;">Loading map...</div>
      <div style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; z-index: -1; visibility: hidden;">
          <div style="position: absolute; left: 0px; top: 0px; width: 1e+07px; height: 1e+07px;"></div>
      </div>
-         <div style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; z-index: -1; visibility: hidden;"><div style="position: absolute; left: 0px; top: 0px; width: 200%; height: 200%;"></div></div><div class="mapsvg-buttons right" style="display: none;"><div class="mapsvg-btn-group"><div class="mapsvg-btn-map mapsvg-in"></div><div class="mapsvg-btn-map mapsvg-out"></div></div><div class="mapsvg-btn-map mapsvg-btn-location"></div></div><div class="mapsvg-tooltip mapsvg-tt-bottom-right" style="min-width: 100px; left: 374px; top: 313px;"><!-- Region fields are available in this template -->
-        DO-21 - San Cristóbal</div></div><div class="mapsvg-details-container" style="width: 100%; background-color: rgba(255, 255, 255, 0.9);"></div></div><div class="mapsvg-sidebar mapsvg-top-container mapsvg-sidebar-right" style="flex-basis: 250px; display: none;"></div></div><div class="mapsvg-footer mapsvg-top-container" style="flex-basis: auto; height: auto; display: none;"></div>
+     <div style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; z-index: -1; visibility: hidden;">
+         <div style="position: absolute; left: 0px; top: 0px; width: 200%; height: 200%;"></div>
+     </div>
+    <div class="mapsvg-buttons right" style="display: none;">
+        <div class="mapsvg-btn-group">
+            <div class="mapsvg-btn-map mapsvg-in"></div>
+            <div class="mapsvg-btn-map mapsvg-out"></div>
+        </div>
+        <div class="mapsvg-btn-map mapsvg-btn-location"></div>
+    </div>
+                    <div class="mapsvg-tooltip mapsvg-tt-bottom-right" v-if="currentProvince" v-bind:style="{left: mouseX + 'px', top: mouseY + 'px'}" style="position:absolute;min-width: 100px; left: 374px; top: 313px;"><!-- Region fields are available in this template -->
+                        {{currentProvince.title}} - infectados: {{ currentProvince.cases }}
+                    </div>
+                </div>
+                <div class="mapsvg-details-container" style="width: 100%; background-color: rgba(255, 255, 255, 0.9);"></div>
+            </div>
+            <div class="mapsvg-sidebar mapsvg-top-container mapsvg-sidebar-right" style="flex-basis: 250px; display: none;"></div>
+        </div>
+        <div class="mapsvg-footer mapsvg-top-container" style="flex-basis: auto; height: auto; display: none;">
+        </div>
     </div>
 </template>
 
@@ -28,7 +56,13 @@
         data() {
             return {
                 provinces: [],
-                hover: false
+                showToolTip: false,
+                SVGmouseTip: null,
+                mouseX: 0,
+                mouseY: 0,
+                mouseOffSetX: 400,
+                mouseOffSety: 300,
+                currentProvince: null
             }
         },
         methods: {
@@ -37,15 +71,47 @@
             },
             getStyle(province) {
                 return !province.hover ? province.style : province.styleFilled
+            },
+            mouseIsMoving(e){
+                this.mouseX = e.pageX - 400;
+                this.mouseY = e.pageY - 300;
+            },
+            mouseOver(province) {
+                province.hover = true;
+                this.currentProvince = province;
+            },
+            mouseLeave(province) {
+                province.hover = false;
+                this.currentProvince = null;
             }
         },
 
         mounted() {
             this.loadPaths();
+            const svg = document.querySelector("#map-svg");
+            svg.addEventListener('mousemove',this.mouseIsMoving);
         }
     }
 </script>
 
 <style scoped>
-
+    .mapsvg-tooltip {
+        font-weight: 400;
+        font-size: 12px;
+        font-family: Helvetica,Arial;
+        color: #000;
+        border-radius: 4px;
+        -moz-border-radius: 4px;
+        -webkit-border-radius: 4px;
+        background-color: #fff;
+        border: 1px solid #eee;
+        padding: 4px 7px;
+        -webkit-transition: opacity .16s ease-out;
+        -moz-transition: opacity .16s ease-out;
+        -ms-transition: opacity .16s ease-out;
+        -o-transition: opacity .16s ease-out;
+        transition: opacity .16s ease-out;
+        pointer-events: none;
+        text-align: left;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,16 +7,8 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import * as VueGoogleMaps from 'vue2-google-maps'
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    libraries: 'places'
-  }
-})
 
 Vue.use(VueAxios, axios)
-
 
 Vue.config.productionTip = false
 
