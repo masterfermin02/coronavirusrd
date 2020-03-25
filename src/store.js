b7d1a392fdeed4d1d04e4eaa04f11c66197db047
masterfermin02 <fperdomo@intellisys.com.do>
2020-03-24 20:03:05 -0400

    Ticket-23 - read all the data from firebase

--- src/store.js
+++ src/store.js
@@ -4,16 +4,26 @@ import provincePath from './services/provincePath.js'
 
 Vue.use(Vuex)
 
-provincePath.getFromFisebase(provinces => store.commit('setProvinces', provinces) )
+provincePath.getFromFisebase(provinces => store.commit('setProvinces', provinces))
+provincePath.getProvincesStat( provincesStat => store.commit('setProvincesStat', provincesStat))
 
 export const store = new Vuex.Store({
   state: {
-    provinces: []
+    provinces: [],
+    provincesStat: {
+      cases: 0,
+      investigations: 0,
+      deaths: 0,
+      recoverers: 0
+    }
   },
 
   mutations: {
     setProvinces(state, val) {
       state.provinces = val
+    },
+    setProvincesStat(state, val) {
+      state.provincesStat = val
     }
   },
 
