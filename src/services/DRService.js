d0341ded352b1193bb5574c60ac35fd8e3d60835
masterfermin02 <fperdomo@intellisys.com.do>
2020-03-24 20:07:40 -0400

    Remove unused http services

--- src/services/DRService.js
+++
@@ -1,8 +0,0 @@
-import CoronaService from './CoronaService'
-
-export default {
-    async getStat() {
-        const { data } = await CoronaService.getLatestStatByCountry("?country=dominican%20republic");
-      return data;
-    }
-}
