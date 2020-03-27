a402414aa855e5af75627eb8641e8dc9be29ef40
masterfermin02 <fperdomo@intellisys.com.do>
2020-03-27 18:26:49 -0400

    Added the share this feature

--- src/filters/provinces.js
+++ src/filters/provinces.js
@@ -1,4 +1,4 @@
-import {greaterThanTo} from "../tools/comparision";
+import { greaterThanTo } from "../tools/comparision";
 import {useWith, filterWith, getWith} from "../tools/functional";
 
 export const within0Cases = useWith(greaterThanTo(0), getWith('cases'))
