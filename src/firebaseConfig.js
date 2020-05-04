05ee431775350bc762bf5811b76bfda611dee8a2
masterfermin02 <fperdomo@intellisys.com.do>
2020-05-04 19:56:37 -0400

    Added env file so travis can handle

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,16 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 // firebase init goes here
-import { config } from './firebaseEnv';
+const config = {
+    apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_BASEURL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_ID,
+    appId: process.env.VUE_APP_FIREBASE_APPID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENTID
+};
 
 firebase.initializeApp(config)
 
